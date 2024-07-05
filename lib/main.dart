0c4fb3940b2cdd65b899620058b454663116d506
haykalgibran <hykalgibran17@gmail.com>
2024-07-05 21:36:13 +0700

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:the_quran/screens/splash_screen.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:the_quran/screens/home_screen.dart';
 import 'package:the_quran/screens/theme.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -24,10 +26,56 @@ class SplashScreen extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                       fontSize: 28),
                 ),
+                const SizedBox(
+                  height: 16,
+                ),
                 Text(
-                  'Learn Quran and Recite once everyday',
+                  'Learn Quran and\nRecite once everyday',
                   style: GoogleFonts.poppins(fontSize: 18, color: text),
                   textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 48,
+                ),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Container(
+                      height: 450,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: const Color(0XFF672CBC)),
+                      child: SvgPicture.asset('assets/svgs/splash.svg'),
+                    ),
+                    Positioned(
+                      left: 0,
+                      bottom: -23,
+                      right: 0,
+                      child: Center(
+                        child: GestureDetector(
+                          behavior: HitTestBehavior.opaque,
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const HomeScreen(),
+                            ));
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 16),
+                            decoration: BoxDecoration(
+                                color: orange,
+                                borderRadius: BorderRadius.circular(32)),
+                            child: Text(
+                              'Get Started',
+                              style: GoogleFonts.poppins(
+                                  fontSize: 18, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ],
             ),
