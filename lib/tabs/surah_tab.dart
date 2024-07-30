b0b1bcd43673f6c3cd953d2ef53516043c745fc7
haykalgibran <hykalgibran17@gmail.com>
2024-07-30 10:38:31 +0700

    Update Surah Page

--- lib/tabs/surah_tab.dart
+++ lib/tabs/surah_tab.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:the_quran/models/surah.dart';
+import 'package:the_quran/screens/theme.dart';
 
 class SurahTab extends StatelessWidget {
   const SurahTab({super.key});
@@ -28,10 +30,81 @@ class SurahTab extends StatelessWidget {
         }));
   }
 
-  Container _surahItem({required Surah surah}) => Container(
-        child: Text(
-          'OKE',
-          style: GoogleFonts.poppins(color: Colors.white),
+  Widget _surahItem({required Surah surah}) => Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: Row(
+          children: [
+            Stack(
+              children: [
+                SvgPicture.asset('assets/svgs/nomor-surah.svg'),
+                SizedBox(
+                  height: 36,
+                  width: 36,
+                  child: Center(
+                    child: Text(
+                      "${surah.nomor}",
+                      style: GoogleFonts.poppins(
+                          color: Colors.white, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  surah.namaLatin,
+                  style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      surah.tempatTurun.name,
+                      style: GoogleFonts.poppins(
+                          color: text,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Container(
+                      width: 4,
+                      height: 4,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(2), color: text),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "${surah.jumlahAyat} Ayat",
+                      style: GoogleFonts.poppins(
+                          color: text,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12),
+                    ),
+                  ],
+                )
+              ],
+            )),
+            Text(
+              surah.nama,
+              style: GoogleFonts.amiri(
+                  color: purple, fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       );
 }
